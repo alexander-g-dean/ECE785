e7fe0440bbd3183372139d52338e03700a4401dd
Alexander G. Dean <alexander.g.dean@gmail.com>
2026-03-16 12:12:18 -0400

    Added g argument to enable toggling within gpioset

--- BES/GPIO-Out/Shell/shell_gpiod_flash.sh
+++ BES/GPIO-Out/Shell/shell_gpiod_flash.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 #  Short script to toggle a GPIO pin with gpiod at the highest frequency
-while true; do
-    gpioset -t0 GPIO17=1
-    gpioset -t0 GPIO17=0
-done
+
+# one argument: g
+if [[ $# -gt 0 && $1 = "g" ]] ; then
+    while true; do
+	gpioset -t1 GPIO17=1
+    done
+else
+    # no arguments (or more than one)
+    while true; do
+	gpioset -t0 GPIO17=1
+	gpioset -t0 GPIO17=0
+    done
+fi
+
+
+      
