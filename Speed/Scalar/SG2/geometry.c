dd784831d04c3f61c91517ed1086924ae284487c
Alexander G. Dean <alexander.g.dean@gmail.com>
2026-04-09 10:51:11 -0400

    Changed VER to command-line def., added version 14

--- Speed/Scalar/SG2/geometry.c
+++ Speed/Scalar/SG2/geometry.c
@@ -1,4 +1,5 @@
 #include "geometry.h"
+#include <stdint.h>
 #include <math.h>
 #include <string.h>
 #define PI 3.14159265f
@@ -6,7 +7,8 @@
 
 extern const PT_T waypoints[];
 
-#define VER 13
+// Now defining VER as command line option to gcc in Makefile
+// old: #define VER 13
 
 int version = VER; // make visible to main
 
@@ -14,6 +16,7 @@ int version = VER; // make visible to main
 11: radians in table, precalc'd sin, cos
 12: Calc_Closeness
 13: Don't do bearing
+14: Don't call strcmp, but use uint32 comparison to detect END instead
 */
 
 // Table holds precalculated sin/cos for p2. Table Lat/Lon values are in radians
@@ -42,7 +45,7 @@ float Calc_Bearing( PT_T * p1,  const PT_T * p2){
   return angle;
 }
 
-#if (VER==12) || (VER==13)
+#if (VER>=12)
 float Calc_Closeness( PT_T * p1,  const PT_T * p2) { 
   // calculates closeness (decreases as distance increases) of locations
 
@@ -61,8 +64,18 @@ void Find_Nearest_Waypoint(float cur_pos_lat, float cur_pos_lon, float * distanc
 		
   int i=0, closest_i=0;
   PT_T ref;
-  float d, b=0, c, max_c=0, closest_d=1E10;
+  float d, b=0;
 
+#if VER<=11
+   float closest_d=1E10;
+#else
+   float c, max_c=0;
+#endif
+#if VER>=14
+  char end_char[] = "END\0";
+  uint32_t END_uint32 = *((uint32_t *) end_char);
+#endif
+  
   *distance = 0.0f;
   *bearing = 0;
   *name = '\0';
@@ -73,8 +86,13 @@ void Find_Nearest_Waypoint(float cur_pos_lat, float cur_pos_lon, float * distanc
   ref.CosLat = cosf(ref.Lat);
 		
   strcpy(ref.Name, "Reference");
-
-  while (strcmp(waypoints[i].Name, "END")) {
+  while (
+#if VER<14
+	 strcmp(waypoints[i].Name, "END")
+#else
+    (*((uint32_t *) waypoints[i].Name) != END_uint32)
+#endif
+    ) {
 #if VER==11
     d = Calc_Distance(&ref, &(waypoints[i]) );
     // if we found a closer waypoint, remember it and display it
@@ -105,7 +123,7 @@ void Find_Nearest_Waypoint(float cur_pos_lat, float cur_pos_lon, float * distanc
   d = acosf(max_c)*6371; // finish distance calcuation
 #endif	
 
-#if VER==13
+#if VER>=13
   b = Calc_Bearing(&ref, &(waypoints[closest_i]) );
 #endif
 
